64b6e61bd3e679d6ab29a56f50f1a2a3e1b868be
thirumal17 <47926024+thirumal17@users.noreply.github.com>
2019-03-15 01:07:31 +0530

    add one sysout

--- Corejava/src/Task1.java
+++ Corejava/src/Task1.java
@@ -4,7 +4,10 @@ public class Task1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("corejava tasks");
+		
 		System.out.println("corejava tasks2");
+		System.out.println("corejava tasks2");
+		
 		
 
 	}
