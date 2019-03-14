8ca60d9314b46f56dac161d08c49abcda524dbc3
thirumal17 <47926024+thirumal17@users.noreply.github.com>
2019-03-15 00:48:55 +0530

    updated2

--- Corejava/src/Task1.java
+++ Corejava/src/Task1.java
@@ -4,6 +4,8 @@ public class Task1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("corejava tasks");
+		System.out.println("corejava tasks2");
+		
 
 	}
 
